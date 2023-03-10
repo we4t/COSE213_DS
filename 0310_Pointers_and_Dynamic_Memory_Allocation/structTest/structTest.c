9a753070354e401a4958a9be3736e7e37ff533a7
we4t <andrew0112@naver.com>
2023-03-10 11:56:14 +0900

    used clang-format

--- 0310_Pointers_and_Dynamic_Memory_Allocation/structTest/structTest.c
+++ 0310_Pointers_and_Dynamic_Memory_Allocation/structTest/structTest.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
-struct STU{
-    int age;
-    char name[10];
+struct STU {
+  int age;
+  char name[10];
 };
 
-int main(void)
-{
-    struct STU kim = {10, "dd"};
-    struct STU kimp = {0,};
-    
-    kimp = kim;
-    kimp.age = 100;
-    printf("%d", kim.age); //not a shallow copy, deep copy.
+int main(void) {
+  struct STU kim = {10, "dd"};
+  struct STU kimp = {
+      0,
+  };
 
-    return 0;
+  kimp = kim;
+  kimp.age = 100;
+  printf("%d", kim.age); // not a shallow copy, deep copy.
+
+  return 0;
 }
