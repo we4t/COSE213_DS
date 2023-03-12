9551c94e1cf7016c7c49cc80cfa88ea8449310fc
we4t <andrew0112@naver.com>
2023-03-12 21:56:57 +0900

    made more folders

--- 0310_Pointers_and_Dynamic_Memory_Allocation/6-structTest/structTest.c
+++ 0310_Pointers_and_Dynamic_Memory_Allocation/6-structTest/structTest.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 
-struct STU {
-  int age;
-  char name[10];
+struct student {
+	int number;
+	char name[10];
+	double grade;
 };
 
-int main(void) {
-  struct STU kim = {10, "dd"};
-  struct STU kimp = {
-      0,
-  };
+struct pstudent{
+	int number;
+	char name[10];
+	double grade;
+} s = {24, "kim", 4.3}; // 전역변수로 선언 가능.
 
-  kimp = kim;
-  kimp.age = 100;
-  printf("%d", kim.age); // not a shallow copy, deep copy.
-
-  return 0;
+int main(int argc, char *argv[]) {
+    // struct student s;
+	// s = {24, "Kim", 4.3};
+	// 이렇게 선언, 정의할 수 없음.
+	struct student s = {25, "123", 3.5};
+	// name[10] : "1|2|3|\0"
+	struct student scopy = {0,};
+	scopy = s; // deep copy;
+	scopy.grade = 4.0/11;
+	printf("scopy.grade was modified, and its grade : %lf\n", scopy.grade);
+	printf("s.grade : %lf\n", s.grade);
+	//객체끼리 shallow copy 가능.
+	
+	return 0;
 }
---
+++ 0310_Pointers_and_Dynamic_Memory_Allocation/6-structTest/typedefT.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct {
+	int age;
+	char name[10];
+} STU;
+
+typedef struct{
+	STU dex[10];
+} sDEX;
+
+int main(int argc, char *argv[]) {
+    /*write your code here*/ 	
+    return 0;
+}
